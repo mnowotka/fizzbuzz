b9d16633197e634c0a19275f99065ab535faafdb
mnowotka <mmmnow@gmail.com>
2019-05-14 19:42:03 +0200

    Infinite loop added.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -16,4 +16,8 @@ def fizzbuzz():
 
 
 if __name__ == '__main__':
-    fizzbuzz()
+    while True:
+        fizzbuzz()
+        shall_continue = input('Print again?')
+        if shall_continue.lower != 'y':
+            break
